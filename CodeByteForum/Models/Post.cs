52c917342112a1caea9d350bdfa35f3aabc51627
Thposbsmt <thposbsmt@protonmail.com>
2020-01-21 16:16:58 +0300

    first init

---
+++ CodeByteForum/Models/Post.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeByteForum.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public User Author { get; set; }
+
+        public List<string> Tags { get; set; }
+
+        public int AnswerCount { get; set; }
+
+        public List<Answer> Answers { get; set; }
+
+        public string Topic { get; set; }
+
+        public List<File> Files { get; set; }
+
+        public TextFile TextFile { get; set; }
+
+        public string Division { get; set; }
+
+        public int ViewsCount { get; set; }
+
+        public bool isSolved { get; set; }
+
+    }
+}
