1ecfc71acc0ff4235caf5d1587838e13b96728fe
Kirill Kholodenko <thposbsmt@protonmail.com>
2020-02-05 18:17:27 +0300

    add rich text editor for create post

---
+++ CodeByteForum/wwwroot/js/text-editor.js
@@ -0,0 +1,43 @@
+﻿let textarea = document.getElementById("text");
+
+function pasteTag(word) {
+    if (word == 'b') {
+        textarea.value += "<b></b>";
+        setCaretToPos(textarea, textarea.value.length - 4);
+    }
+    if (word == 'h1') {
+        textarea.value += "<h1></h1>";
+        setCaretToPos(textarea, textarea.value.length - 5);
+    }
+    if (word == 'h2') {
+        textarea.value += "<h2></h2>";
+        setCaretToPos(textarea, textarea.value.length - 5);
+    }
+    if (word == 'h3') {
+        textarea.value += "<h3></h3>";
+        setCaretToPos(textarea, textarea.value.length - 5);
+    }
+    if (word == 'h4') {
+        textarea.value += "<h4></h4>";
+        setCaretToPos(textarea, textarea.value.length - 5);
+    }
+}
+textarea.addEventListener("keydown", (e) => {
+    switch (e.keyCode) {
+        case 66:
+            if (e.ctrlKey)
+                pasteTag('b');
+            break;
+    }
+});
+function setCaretToPos(input, pos) {
+    setSelectionRange(input, pos, pos);
+}
+
+function setSelectionRange(input, selectionStart, selectionEnd) {
+    if (input.setSelectionRange) {
+        input.focus();
+        input.setSelectionRange(selectionStart, selectionEnd);
+    }
+}
+
